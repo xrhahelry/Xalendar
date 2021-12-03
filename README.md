26d0915b6898bf48c0ebb36a996b036719eb1927
SujalBajra <sujalbajracharya1@gmail.com>
2021-12-03 19:51:16 +0545

    prints full date

---
+++ README.md
@@ -0,0 +1,3 @@
+# Xalender
+#### Terminal calendar for windows written in c
+#### Only prints full date for now
---
+++ main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<time.h>
+
+void date();
+
+void main() {
+ date();
+}
+
+
+void date() {
+  time_t rawtime;
+  struct tm* timeinfo;
+
+  char m[10];
+  char y[10];
+  char wd[10];
+  char md[10];
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  strftime(m, 10, "%b", timeinfo);
+  strftime(y, 10, "%Y", timeinfo);
+  strftime(wd, 10, "%a", timeinfo);
+  strftime(md, 10, "%d", timeinfo);
+
+  printf("%s %s %s %s",y,m,md,wd);
+}
